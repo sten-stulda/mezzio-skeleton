a02611d6b82b9e475ddf43e6f202af348163edef
stulda <stulik@webimage.cz>
2021-10-13 09:16:39 +0200

    Create module User
    composer mezzio module:create User

--- config/config.php
+++ config/config.php
@@ -14,6 +14,7 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
+    \User\ConfigProvider::class,
     \Laminas\I18n\ConfigProvider::class,
     \Laminas\Form\ConfigProvider::class,
     \Laminas\Hydrator\ConfigProvider::class,
