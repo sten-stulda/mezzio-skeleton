0d6bdbf2fbca9e6d406adf9dab171f47ee4ee7eb
stulda <stulik@webimage.cz>
2021-10-13 09:51:10 +0200

    Update routes.php
    /user/register

--- config/routes.php
+++ config/routes.php
@@ -39,5 +39,6 @@ use Psr\Container\ContainerInterface;
 
 return static function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
     $app->get('/', App\Handler\HomePageHandler::class, 'home');
+    $app->get('/user/register', User\Handler\RegisterHandler::class, 'Register');
     $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
 };
