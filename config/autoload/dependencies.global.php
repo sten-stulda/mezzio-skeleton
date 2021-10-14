3bbdebb20b99943b5cb91f1d7636554c51ae717a
stulda <stulik@webimage.cz>
2021-10-14 11:20:36 +0200

    Update dependencies.global.php
    add Laminas-db and setting

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+use Laminas\Db\Adapter\Adapter;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Adapter\AdapterServiceFactory;
+
 return [
     // Provides application-wide services.
     // We recommend using fully-qualified class names whenever possible as
@@ -11,6 +15,7 @@ return [
         // key is the alias name, the value is the service to which it points.
         'aliases' => [
             // Fully\Qualified\ClassOrInterfaceName::class => Fully\Qualified\ClassName::class,
+            Adapter::class => AdapterInterface::class
         ],
         // Use 'invokables' for constructor-less services, or services that do
         // not require arguments to the constructor. Map a service name to the
@@ -21,6 +26,14 @@ return [
         // Use 'factories' for services provided by callbacks/factory classes.
         'factories' => [
             // Fully\Qualified\ClassName::class => Fully\Qualified\FactoryName::class,
+            AdapterInterface::class => AdapterServiceFactory::class
         ],
     ],
+    'db' => [
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=mezzio;hostname=localhost',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'' 
+        ]
+    ]
 ];
