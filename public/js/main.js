852bfb3ea7a0f8e1209eeba37e0b65d089bfed6b
stulda <stulik@webimage.cz>
2021-10-14 11:19:48 +0200

    add main.js

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+function clickedConfidentiality(item) {
+    alert('Confidentiality: ' + $(item).attr("value"));
+}
+
+function clickedIntegrity(item) {
+    alert('Integrity: ' + $(item).attr("value"));
+}
