adff5dee32146b5785b61ba8e2eabe95c26b716e
stulda <stulik@webimage.cz>
2021-10-19 11:24:17 +0200

    update
    Signed-off-by: stulda <stulik@webimage.cz>

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,80 @@
 function clickedConfidentiality(item) {
     $("input:radio[id=Confidentiality]").attr("checked", false)
-    $("input:radio[name=radio_"+$(item).attr("value")+"]").attr("checked", true)
+    $("input:radio[name=radio_" + $(item).attr("value") + "]").attr("checked", true)
 }
 
-function clickedImpact(item){
+function clickedImpact(item) {
     $("input:radio[id=Impact]").attr("checked", false)
-    $("input:radio[name=radio_"+$(item).attr("value")+"]").attr("checked", true)
+    $("input:radio[name=radio_" + $(item).attr("value") + "]").attr("checked", true)
 }
 
 function clickedIntegrity(item) {
     $("input:radio[id=Integrity]").attr("checked", false)
-    $("input:radio[name=radio_"+$(item).attr("value")+"]").attr("checked", true)
+    $("input:radio[name=radio_" + $(item).attr("value") + "]").attr("checked", true)
 }
 
-// Initialize tooltip component
-$(function() {
-    $('[data-toggle="tooltip"]').tooltip()
-})
 
-// Initialize popover component
-$(function() {
+
+
+// /**
+//  * This function search cars by word
+//  *
+//  * @param word
+//  * @returns {Promise<Response>}
+//  */
+// async function searchCar(word) {
+//     let dataSend = new FormData();
+//     dataSend.append('word', word);
+//     return await fetch('http://localhost/car', {
+//         method: 'POST',
+//         body: dataSend
+//     });
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Get input element and add event keyup
+//     let searchInput = document.getElementsByName('setImpact');
+//     searchInput.addEventListener('keyup', async function () {
+//         console.info("Buscando espera por favor...");
+//         let result = await searchCar(searchInput.value);
+//         let cars = await result.json();
+//         cars.forEach(function (car) {
+//             console.log(car);
+//         });
+//     });
+// });
+
+window.onload = function () {
+    $("input:radio[name=setImpact]").on('change', function () {
+        value = $(this).val()
+        console.log("Bylo vybráno hodnocení: " + value);
+    });
+    $("input:radio[name=setVulnerability]").on('change', function () {
+        value = $(this).val()
+        console.log("Bylo vybráno hodnocení: " + value);
+    });
+    $("input:radio[name=setThreat]").on('change', function () {
+        value = $(this).val()
+        console.log("Bylo vybráno hodnocení: " + value);
+    });
+
+    // Initialize tooltip component
+
+    $('[data-toggle="tooltip"]').tooltip()
+
+
+    // Initialize popover component
+
     $('[data-toggle="popover"]').popover()
-})
+
+    $("a[name=setConfidentiality]").on('click', function () {
+        console.log('vybrano');
+    })
+
+}
+
+function setConfidentiality(item) {
+    console.log("Vybrano hodnocení: " + $(item).attr("value"));
+    //$("input:radio[id=Integrity]").attr("checked", false)
+    //$("input:radio[name=radio_" + $(item).attr("value") + "]").attr("checked", true)
+}
